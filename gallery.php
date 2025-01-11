231b838cc1c9a3d5919b7839c3e58f2ffd7d047d
megahicham01 <megahicham01@gmail.com>
2025-01-11 01:46:11 +0100

    Mise à jour du code pour gallery.php, ajoute reference images

--- gallery.php
+++ gallery.php
@@ -216,37 +216,37 @@
           <div class="container">
             <div class="zoom-gallery row justify-content-center">
               <div class="col-lg-8">
-                <a class="gallery_item popup_image" href="assets/images/gallery/gallery_image_7-min.jpg">
-                  <img src="assets/images/gallery/gallery_image_7-min.jpg" alt="Talking Minds - Psychotherapist Site Template">
+                <a class="gallery_item popup_image" href="assets/images/gallery/5.jpg">
+                  <img src="assets/images/gallery/5.jpg" alt="Talking Minds - Psychotherapist Site Template">
                 </a>
               </div>
               <div class="col-lg-4">
                 <div class="row">
                   <div class="col-lg-12 col-md-6 col-sm-6">
-                    <a class="gallery_item popup_image" href="assets/images/gallery/gallery_image_8-min.jpg">
-                      <img src="assets/images/gallery/gallery_image_8-min.jpg" alt="Talking Minds - Psychotherapist Site Template">
+                    <a class="gallery_item popup_image" href="assets/images/gallery/8.jpg">
+                      <img src="assets/images/gallery/8.jpg" alt="Talking Minds - Psychotherapist Site Template">
                     </a>
                   </div>
                   <div class="col-lg-12 col-md-6 col-sm-6">
-                    <a class="gallery_item popup_image" href="assets/images/gallery/gallery_image_9-min.jpg">
-                      <img src="assets/images/gallery/gallery_image_9-min.jpg" alt="Talking Minds - Psychotherapist Site Template">
+                    <a class="gallery_item popup_image" href="assets/images/gallery/9.jpg">
+                      <img src="assets/images/gallery/9.jpg" alt="Talking Minds - Psychotherapist Site Template">
                     </a>
                   </div>
                 </div>
               </div>
               <div class="col-lg-4">
-                <a class="gallery_item popup_image" href="assets/images/gallery/gallery_image_10-min.jpg">
-                  <img src="assets/images/gallery/gallery_image_10-min.jpg" alt="Talking Minds - Psychotherapist Site Template">
+                <a class="gallery_item popup_image" href="assets/images/gallery/10.jpg">
+                  <img src="assets/images/gallery/10.jpg" alt="Talking Minds - Psychotherapist Site Template">
                 </a>
               </div>
               <div class="col-lg-4 col-md-6 col-sm-6">
-                <a class="gallery_item popup_image" href="assets/images/gallery/gallery_image_11-min.jpg">
-                  <img src="assets/images/gallery/gallery_image_11-min.jpg" alt="Talking Minds - Psychotherapist Site Template">
+                <a class="gallery_item popup_image" href="assets/images/gallery/11.jpg">
+                  <img src="assets/images/gallery/11.jpg" alt="Talking Minds - Psychotherapist Site Template">
                 </a>
               </div>
               <div class="col-lg-4 col-md-6 col-sm-6">
-                <a class="gallery_item popup_image" href="assets/images/gallery/gallery_image_3-min.jpg">
-                  <img src="assets/images/gallery/gallery_image_3-min.jpg" alt="Talking Minds - Psychotherapist Site Template">
+                <a class="gallery_item popup_image" href="assets/images/gallery/3.jpg">
+                  <img src="assets/images/gallery/3.jpg" alt="Talking Minds - Psychotherapist Site Template">
                 </a>
               </div>
             </div>
